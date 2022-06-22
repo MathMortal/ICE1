7a9bd3e031a73d878ca31c790f2e0eda1e0fdea7
Aryan Patel <ap007jb742002@gmail.com>
2022-06-21 22:53:25 -0400

    version 54545

---
+++ AN1/src/NA.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author Aryan patel
+ */
+public class NA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        
+        
+        System.out.println("*******************");
+        System.out.println("******************");
+        System.out.println("****************");
+        System.out.println("*************");
+        System.out.println("**********");
+        System.out.println("******");
+        System.out.println("***");
+        System.out.println("*");
+        
+    }
+    
+        // TODO code application logic here
+    }
+
